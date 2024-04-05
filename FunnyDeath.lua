1dd5b8d57a6871df4de943756864b47550a07ad3
Chrono <150987804+ChronoAcceleration@users.noreply.github.com>
2024-04-04 20:16:54 -0700

    Create FunnyDeath.lua

---
+++ FunnyDeath.lua
@@ -0,0 +1,16 @@
+--!server
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Bricks = ReplicatedStorage.Bricks
+local DeathHint = Bricks.DeathHint
+
+local Players = game:GetService("Players")
+local Player = Players.TotallyNotChrono --// My own for ref
+local Character = Player.Character
+local Humanoid = Character.Humanoid
+
+Humanoid:TakeDamage(100)
+DeathHint:FireAllClients("Yellow", {
+  "You stubbed your toe... HARD. The pain shot up your leg, causing a muscle spasm that triggered a nearby earthquake. The resulting tremors dislodged a precariously balanced stack of pickles in a nearby shop, which cascaded down and formed a slippery ramp. A stray squirrel, startled by the pickle avalanche, tripped over the ramp and launched itself into a jetpack-wearing tourist, sending them careening into you at mach speed. Oof. |50",
+  "Lesson learned: Always wear sturdy shoes and be wary of rogue pickles.| 20"
+})
